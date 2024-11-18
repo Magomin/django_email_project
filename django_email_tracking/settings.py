5cacc966980e7304bdecd221f0f7d94f791a22ae
Magomin <matthieu@fribl.co>
2024-11-18 13:18:41 +0100

    changes

--- django_email_tracking/settings.py
+++ django_email_tracking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wg6e=$*idq4an$i^p&cs+z8-9isvq!)q)x%uc(1gl4kf&-^mpv'
+
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key-for-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
